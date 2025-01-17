8e734e9271c61ded64eda0df363022b0ca0f543a
root <root@localhost.localdomain>
2025-01-17 15:09:55 -0500

    ajustes

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,13 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DataApiService } from '@app/services/data-api-service';
+import { GlobalService } from '@app/services/global.service';
+import { virtualRouter } from '@app/services/virtualRouter.service';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.css'
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent {
+  dateRange: string = '';
 
+  constructor(
+    public global: GlobalService,
+    public virtualRoute: virtualRouter,
+    public dataApi: DataApiService
+  ) {
+    this.global.getRepositorios();
+    this.global.getTemas();
+    this.global.getCategories();
+  }
+
+  changeTema(tema: any) {
+    this.global.selectedTema = tema;
+  }
+
+  changeCategory(category: any) {
+    this.global.selectedCategory = category;
+  }
+
+  changeRepositorio(repositorio: any) {
+    this.global.selectedRepositorio = repositorio;
+  }
 }
